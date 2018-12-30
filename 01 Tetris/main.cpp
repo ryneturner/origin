bd0644d1fe53ef59f038ca269703153dc4c39ce8
Andrew Sutton <asutton@lock3software.com>
2018-12-30 09:33:43 -0500

    Restyle formatting.

--- 01 Tetris/main.cpp
+++ 01 Tetris/main.cpp
@@ -5,23 +5,26 @@ using namespace sf;
 const int M = 20;
 const int N = 10;
 
-int field[M][N] = {0};
+int field[M][N] = { 0 };
 
-struct Point {
+struct Point
+{
   int x, y;
 } a[4], b[4];
 
 int figures[7][4] = {
-    1, 3, 5, 7, // I
-    2, 4, 5, 7, // Z
-    3, 5, 4, 6, // S
-    3, 5, 4, 7, // T
-    2, 3, 5, 7, // L
-    3, 5, 7, 6, // J
-    2, 3, 4, 5, // O
+  1, 3, 5, 7, // I
+  2, 4, 5, 7, // Z
+  3, 5, 4, 6, // S
+  3, 5, 4, 7, // T
+  2, 3, 5, 7, // L
+  3, 5, 7, 6, // J
+  2, 3, 4, 5, // O
 };
 
-bool check() {
+bool
+check()
+{
   for (int i = 0; i < 4; i++)
     if (a[i].x < 0 || a[i].x >= N || a[i].y >= M)
       return 0;
@@ -31,7 +34,9 @@ bool check() {
   return 1;
 };
 
-int main() {
+int
+main()
+{
   srand(time(0));
 
   RenderWindow window(VideoMode(320, 480), "The Game!");
